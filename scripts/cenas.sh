7ffbff3f8049577a01d5bb8f5858c3ebc7b3ac3c
nunodsfernandes <75372015+nunodsfernandes@users.noreply.github.com>
2020-12-10 17:21:16 +0000

    Update cenas.sh

--- scripts/cenas.sh
+++ scripts/cenas.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-clear
-docker pull hub.docker.hpecorp.net/cms-sd/eo-core:1.0.4
+docker pull 160256247964.dkr.ecr.us-east-2.amazonaws.com/eo-core:1.0.4
 
-clear
-docker pull hub.docker.hpecorp.net/cms-sd/eo-ui-frontend:1.0.4
+docker pull 160256247964.dkr.ecr.us-east-2.amazonaws.com/eo-ui-frontend:1.0.4
 
-clear
-docker pull hub.docker.hpecorp.net/cms-sd/eo-ui-backend:1.0.4
+docker pull 160256247964.dkr.ecr.us-east-2.amazonaws.com/eo-ui-backend:1.0.4
 
-clear
-docker pull hub.docker.hpecorp.net/cms-sd/sd-ui:3.4.1
+docker pull 160256247964.dkr.ecr.us-east-2.amazonaws.com/sd-ui:3.4.1
 
-clear
-docker pull hub.docker.hpecorp.net/cms-sd/sd-ui-plugin:3.4.1
+docker pull 160256247964.dkr.ecr.us-east-2.amazonaws.com/sd-ui-plugin:3.4.1
 
-clear
-docker pull hub.docker.hpecorp.net/cms-sd/eo-kc-init:1.0.4
+docker pull 160256247964.dkr.ecr.us-east-2.amazonaws.com/eo-kc-init:1.0.4
 
-clear
-docker pull hub.docker.hpecorp.net/cms-sd/grok_exporter:latest
+docker pull 160256247964.dkr.ecr.us-east-2.amazonaws.com/grok_exporter:latest
+
+echo " *********** These are the docker images pulled from ECR:"
+echo ""
+docker images | grep amazonaws
+echo ""
+echo ""
+echo " *********** These are the pods running on the Minikube setup:"
+kubectl get pods --all-namespaces
+echo ""
+echo ""
+echo "Your instance should now be ready for the Helm Chart deployment."
+echo "Refer to the SaaS Ops GitHub page for further instructions!"
+echo ""
+echo "Thank you."
+echo ""
