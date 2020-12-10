4a9db8df7a172a88092c55d957e762b37aa52492
nunodsfernandes <75372015+nunodsfernandes@users.noreply.github.com>
2020-12-10 14:16:13 +0000

    Update cenas.sh

--- scripts/cenas.sh
+++ scripts/cenas.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 clear
-docker pull hub.docker.hpecorp.net/cms-sd/eo-core:1.0.3
+docker pull hub.docker.hpecorp.net/cms-sd/eo-core:1.0.4
 
 clear
-docker pull hub.docker.hpecorp.net/cms-sd/eo-ui-frontend:1.0.3
+docker pull hub.docker.hpecorp.net/cms-sd/eo-ui-frontend:1.0.4
 
 clear
-docker pull hub.docker.hpecorp.net/cms-sd/eo-ui-backend:1.0.3
+docker pull hub.docker.hpecorp.net/cms-sd/eo-ui-backend:1.0.4
 
 clear
-docker pull hub.docker.hpecorp.net/cms-sd/sd-ui:3.4.0
+docker pull hub.docker.hpecorp.net/cms-sd/sd-ui:3.4.1
 
 clear
-docker pull hub.docker.hpecorp.net/cms-sd/sd-ui-plugin:3.4.0
+docker pull hub.docker.hpecorp.net/cms-sd/sd-ui-plugin:3.4.1
 
 clear
-docker pull hub.docker.hpecorp.net/cms-sd/eo-kc-init:1.0.3
+docker pull hub.docker.hpecorp.net/cms-sd/eo-kc-init:1.0.4
 
 clear
 docker pull hub.docker.hpecorp.net/cms-sd/grok_exporter:latest
