6c1a8b0b7542402b3f085b72d5fcb5c5840b457f
nunodsfernandes <75372015+nunodsfernandes@users.noreply.github.com>
2020-12-10 13:57:48 +0000

    Create cenas.sh

---
+++ scripts/cenas.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+docker pull hub.docker.hpecorp.net/cms-sd/eo-core:1.0.3
+
+clear
+docker pull hub.docker.hpecorp.net/cms-sd/eo-ui-frontend:1.0.3
+
+clear
+docker pull hub.docker.hpecorp.net/cms-sd/eo-ui-backend:1.0.3
+
+clear
+docker pull hub.docker.hpecorp.net/cms-sd/sd-ui:3.4.0
+
+clear
+docker pull hub.docker.hpecorp.net/cms-sd/sd-ui-plugin:3.4.0
+
+clear
+docker pull hub.docker.hpecorp.net/cms-sd/eo-kc-init:1.0.3
+
+clear
+docker pull hub.docker.hpecorp.net/cms-sd/grok_exporter:latest
